b161cda2d05a02467754ed8f860c269f2674dc49
Sudipto Saha <sahasudipto336@gmail.com>
2024-02-27 06:29:27 +0530

    Add extension method demo

---
+++ Extension_Method_Demo/IntExtenstions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension_Method_Demo
+{
+    internal static class IntExtenstions
+    {
+        public static bool IsGreaterThan (this int caller, int param)
+        {
+            return caller > param;
+        }
+    }
+}
---
+++ Extension_Method_Demo/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Extension_Method_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int caller = 10;
+            int param = 20;
+
+            bool isCallerGreatherThanParam = caller.IsGreaterThan(param);
+
+            Console.WriteLine($"Is extension method caller ({caller}) greater than param ({param})? - {isCallerGreatherThanParam}");
+        }
+    }
+}
