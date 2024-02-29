83fa9f7aae7f85843cfdb28e2d7dfb6841a12ab8
Sudipto Saha <sahasudipto336@gmail.com>
2024-02-29 22:13:50 +0530

    Added multithreading demo

---
+++ Multithreading_Demo/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Multithreading_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(BasicThreadFn);
+            thread.Start();
+        }
+
+        static void BasicThreadFn() { Console.WriteLine("Thread fn executed"); }
+    }
+}
