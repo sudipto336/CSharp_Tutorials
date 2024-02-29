becd34de783899d7b6cbbd19b8ce87e970359086
Sudipto Saha <sahasudipto336@gmail.com>
2024-02-29 23:38:11 +0530

    Update multithreading demo

--- Multithreading_Demo/Program.cs
+++ Multithreading_Demo/Program.cs
@@ -4,10 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Thread thread = new Thread(BasicThreadFn);
-            thread.Start();
+            Thread thread1 = new Thread(BasicThreadFn);
+
+            //Or
+
+            ThreadStart threadStart = new ThreadStart(BasicThreadFn2);
+
+            Thread thread2 = new Thread(threadStart);
+
+            thread1.Start();
+
+            thread2.Start();
         }
 
         static void BasicThreadFn() { Console.WriteLine("Thread fn executed"); }
+
+        static void BasicThreadFn2() { Console.WriteLine("Thread2 fn executed"); }
+
+        static void BasicParameterizedThreadFn(Object num) { Console.WriteLine("Thread2 fn executed"); }
     }
 }
