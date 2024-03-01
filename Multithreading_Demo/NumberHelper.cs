84594b145f9a5abd307e4966c0ebc737fdbe89c3
Sudipto Saha <sahasudipto336@gmail.com>
2024-03-01 05:55:53 +0530

    Add demo for passing data to thread fn in type safe manner

---
+++ Multithreading_Demo/NumberHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multithreading_Demo
+{
+    internal class NumberHelper
+    {
+        private int _num;
+
+        public NumberHelper(int num)
+        {
+            _num = num;
+        }
+
+        public void TypeSafeThreadFn()
+        {
+            Console.WriteLine($"Data passed to thread fn executed in type safe manner with value {_num}");
+        }
+    }
+}
--- Multithreading_Demo/Program.cs
+++ Multithreading_Demo/Program.cs
@@ -21,11 +21,19 @@
 
             
             
-            //Thread method with param
+            //Thread method with param (not type safe)
 
-            ParameterizedThreadStart parameterizedThreadStart = new ParameterizedThreadStart(BasicParameterizedThreadFn);
+            ParameterizedThreadStart parameterizedThreadStart = new ParameterizedThreadStart(NotTypeSafeParameterizedThreadFn);
 
             Thread thread3 = new Thread(parameterizedThreadStart);
+
+            //Passing data to thread fn in type safe manner
+
+            NumberHelper numberHelper = new NumberHelper(6);
+
+            ThreadStart threadStart3 = new ThreadStart(numberHelper.TypeSafeThreadFn);
+
+            Thread thread4 = new Thread(threadStart3);
 
             
             
@@ -35,16 +43,25 @@
 
             thread2.Start();
 
+            //data not passed type safely
+
             thread3.Start(5);
+
+            //data passed type safely
+
+            thread4.Start();
         }
 
-        static void BasicThreadFn() { Console.WriteLine("Thread fn executed"); }
+        static void BasicThreadFn() { Console.WriteLine("Thread1 fn executed"); }
 
         static void BasicThreadFn2() { Console.WriteLine("Thread2 fn executed"); }
 
-        static void BasicParameterizedThreadFn(Object num) { 
+        static void NotTypeSafeParameterizedThreadFn(Object num)
+        {
+
+            //not type safe thread fn as if data of type other than int sent, then runtime execption will occur
             int param = Convert.ToInt32(num);
-            Console.WriteLine($"Thread23 fn executed with param {param}");
+            Console.WriteLine($"Not type safe parameterized thread fn executed with param {param}");
         }
     }
 }
